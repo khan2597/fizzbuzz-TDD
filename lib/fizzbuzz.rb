494861cd6b4eadf723dbfe9b78e728ec965ce4ff
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-10-07 16:28:02 +0100

    added tests for fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,11 @@
 def fizzbuzz(num)
-	p 'fizz'
+	fizz = "fizz"
+	buzz = "buzz"
+	if number % 3 == 0 && number % 5 != 0
+		fizz
+	elsif number % 5 == 0 && number % 3 != 0
+		buzz
+	elsif number % 3 == 0 && number % 5 == 0
+		fizz + buzz
+	end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,13 @@
 require './lib/fizzbuzz'
 
 describe 'fizzbuzz' do
-	it 'returns "fizz" when passed 3' do
+	it 'returns "fizz" when passed a multiple of 3' do
 		expect(fizzbuzz(3)).to eq 'fizz'
+	end
+	it 'returns "buzz" when passed a multiple of 5' do
+		expect(fizzbuzz(5)).to eq 'buzz'
+	end
+	it 'returns "fizzbuzz" when passed a multiple of 3 & 5' do
+		expect(fizzbuzz(15)).to eq 'fizz'
 	end
 end
